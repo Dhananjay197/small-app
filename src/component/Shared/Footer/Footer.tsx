ec59c25cde6490942828d76a00bd50e269175480
dhananjay197 <dhananjay231217@gmail.com>
2022-10-06 20:21:56 +0530

    footer

--- src/component/Shared/Footer/Footer.tsx
+++ src/component/Shared/Footer/Footer.tsx
@@ -5,7 +5,9 @@ const Footer = () => {
 <footer>
     <div className="footer-main">
         <div className="container">
-            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <img src={} alt="logo" 
+            </div>
             <div className="col-md-2"></div>
             <div className="col-md-2"></div>
             <div className="col-md-2"></div>
